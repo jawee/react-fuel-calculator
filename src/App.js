1145751cb1da0b3d3e51bb53bc862cc8b6314466
Andreas Olsson <olsson.andreas@gmail.com>
2020-08-06 16:57:43 +0200

    Styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
 
 function App() {
   const [timeLeft, setTimeLeft] = useState(0);
